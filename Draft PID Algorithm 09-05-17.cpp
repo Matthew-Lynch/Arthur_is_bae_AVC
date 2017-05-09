e13e3fea202095d6390948835c189c1202026da0
ConaghFM <c.fitzgerald-mansell@live.com>
2017-05-09 22:27:45 +1200

    Fixed typos, added notes

--- Draft PID Algorithm 09-05-17.cpp
+++ Draft PID Algorithm 09-05-17.cpp
@@ -1,6 +1,5 @@
-
-double pid(double input){
-	double dt = 0.001//time step in s
+double pid(double input){ //call pid passing error signal as input
+	double dt = 0.001 //time step in s
 	float dGain = 0.5; //differential gain co-efficient
 	float iGain = 0.5; //integral gain co-efficient
 	float pGain = 0.5; //proportional gain co-efficient
@@ -12,19 +11,16 @@ double pid(double input){
 	double PID;
 
 	errNew=input;
-	sleep1(0, 001);//this must equal dt
+	sleep1(0, 001); //this MUST equal dt
 
 while(true){
 	errOld=errNew;
 	errNew=input;
 	diff = ((errNew-errOld)/dt)*dGain; //change in err over time step * gain
 	integ = ((errNew+errOld)/2)*dt*iGain; //average err * time step * gain
-	sleep1(0, 001);//this must equal dt
-	prop = errNew*pGain;
-	PID = prop+diff+integ;
+	sleep1(0, 001); //this MUST equal dt
+	prop = errNew*pGain; //Calculate proportional err
+	PID = prop+diff+integ; //Sum components
 	return PID;
 }
-}
-
-
 }
