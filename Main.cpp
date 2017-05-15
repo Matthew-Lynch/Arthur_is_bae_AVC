ec68a30361ea7628d831bb6133261a5289a057d5
ConaghFM <c.fitzgerald-mansell@live.com>
2017-05-15 13:48:59 +1200

    Main
    Draft commit for main();

---
+++ Main.cpp
@@ -0,0 +1,25 @@
+void main()
+bool cam = true;
+double errNear;
+double errFar;
+double errPID;
+int driveLeft;
+int driveRight;
+
+while(true){
+cam = set_mode()
+if(cam=true){
+	near = read_cam();
+	far = read_cam();
+}
+else{
+	read_ir(); //write processes for this
+}
+errPID = pid(errNear, errFar);
+driveLeft = scale_pid(errPID, 0.95); // tune gain L
+driveRight = scale_pid(errPID, 1); //tune gain R
+
+set_motor(0, driveLeft);
+set_motor(1, driveRight);
+
+}
