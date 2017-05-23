9d38b4713a7985e044115ce11f0f7908a8bc0978
ConaghFM <c.fitzgerald-mansell@live.com>
2017-05-24 10:28:05 +1200

    Removed odd declarations, fixed ip array size

--- Gate Command.cpp
+++ Gate Command.cpp
@@ -1,10 +1,10 @@
 bool open_gate(){
 
-	char ip[24] = {1,3,0,1,9,5,0,0,6,1,9,6}; // 130.195.6.196
+	char ip[15] = {1,3,0,1,9,5,0,0,6,1,9,6}; // 130.195.6.196
 	char message[24] = {p,l,e,a,s,e}; // "please"
 	
-	int connect_to_server(ip, 1024);
-	int send_to_server(message);
+	connect_to_server(ip, 1024);
+	send_to_server(message);
 	receive_from_server(char code[24]);
 	send_to_server(code);
 	
