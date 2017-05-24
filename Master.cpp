0576fc0b45e804f187cb16e1c011e574cd21d339
ConaghFM <c.fitzgerald-mansell@live.com>
2017-05-25 09:34:26 +1200

    Update 25/05
    Update to reflect code currently on the Pi. 
    Opens the gate
    Sometimes got to the end of Q2 before "if(whiteCount > 320/4*3 || whiteCount < 7)" changed to "if(whiteCount < 7){"

--- Master.cpp
+++ Master.cpp
@@ -4,15 +4,16 @@
 
 //Global Variables
 int whiteCount = 0;
-int baseSpeed = 70;
+int baseSpeed = 40;
 
 
 int read_cam(int row){
-	int sum, p, white, col = 0, sum = 0; //declare variables
+	whiteCount = 0;
+	int p, white, col = 0, sum = 0; //declare variables
 	take_picture();
 
 	for(int i=0; i<320 ; i++){ 	//Camera reading Near row
-		white = get_pixel(col+i,row,3);
+		white = get_pixel(row,col+i,3);
 		if (white>127){
 		p = 1; // for white
 		whiteCount++; //increase global variable whiteCount
@@ -20,19 +21,20 @@ int read_cam(int row){
 		else {
 		p = 0; // for black
 		}
-		sum = sum + (i-160)*p; //Gives the white a value that is centred at 0 (middle of picture)
+		sum += (i-160)*p; //Gives the white a value that is centred at 0 (middle of picture)
 		}
 		
 	return sum; //Return error signal;
 }
 
 double scale_pid(double pid, float motorGain){ //call scale passing pid signal and motor gain as input
-	float scale = 0.2; // Needs tuning based on max PID output
+	
+	float scale = 0.002; // Needs tuning based on max PID output
 	double temp; // Storeage variable for gain processing
 	int output;
 	
-	if(pid * scale > 254){ //Limiter
-		temp = 254; 
+	if(pid * scale > 100){ //Limiter
+		temp = 100; 
 	}
 	else{
 		temp = (pid * scale); //scales PID and casts to int
@@ -43,9 +45,9 @@ double scale_pid(double pid, float motorGain){ //call scale passing pid signal a
 }
 
 double pid(double inNear,double inFar){ //call pid passing error signal as input
-	float dGain = 1.0; //differential gain co-efficient
+	float dGain = 0.5; //differential gain co-efficient
 	float iGain = 0.1; //integral gain co-efficient
-	float pGain = 0.3; //proportional gain co-efficient
+	float pGain = 0.7; //proportional gain co-efficient
 	double errTotal = 0; //recent error signal input
 	double diff; //differential
 	double integ; //integral
@@ -67,6 +69,27 @@ double pid(double inNear,double inFar){ //call pid passing error signal as input
 	}
 }
 
+void goBack(int left, int right){
+	
+	set_motor(1, -40-left);
+	set_motor(2, -40+right);	
+}
+
+bool open_gate(){
+
+	char ip[15] = {'1','3','0','.','1','9','5','.','6','.','1','9','6'};// 130.195.6.196
+	char message[24] = {'P', 'l', 'e', 'a', 's', 'e'};
+	char code[24];
+	
+	connect_to_server(ip,1024);
+	printf("ip=%s\n",ip);
+	send_to_server(message);  
+	printf("message=%s\n",message);
+	receive_from_server(code);
+	send_to_server(code);
+	//sleep1(0, 1); delay if needed
+	return true;
+}
 
 
 int main(){
@@ -77,8 +100,14 @@ int main(){
 	double errPID;
 	int driveLeft;
 	int driveRight;
+	int joe = 0;
+	int lost = 0;
 	
-	while(true){
+	//sleep1(15, 0);
+	open_gate();
+	sleep1(1, 0);
+	while(joe<3000){
+		
 		//cam = set_mode();
 		/* whiteCount = whiteCount/2; // averages the two whitecount readings
 		 * if(whiteCount<3){
@@ -89,6 +118,7 @@ int main(){
 		 * }
 		 * whiteCount = 0; // reset whitecount for next tick
 		**/
+		//display_picture(1, 0);
 		if(cam==true){
 			errNear = read_cam(160);
 			errFar = read_cam(80);
@@ -97,12 +127,31 @@ int main(){
 			//read_ir(); //write processes for this
 		}
 		errPID = pid(errNear,errFar);
+		
+		
 		driveLeft = scale_pid(errPID, 1); // tune gain L
 		driveRight = scale_pid(errPID, 1); // tune gain R
-
-		set_motor(0, baseSpeed+driveLeft);
-		set_motor(1, baseSpeed+driveRight);
-		//sleep1(0, 000010); //sleep code for stability?
+		sleep1(0, 01);
+		
+		if(whiteCount < 7){
+			lost++;
+			goBack(driveLeft, driveRight);
+			sleep1(0, 1);
+			if(lost>10){
+				goBack(driveLeft, driveRight);
+				sleep1(0, 2);
+				lost=0;
+			}
 		}
+		else{
+			set_motor(2, baseSpeed+driveLeft);
+			set_motor(1, baseSpeed-driveRight);
+		}
+		
+		
+		//sleep1(0, 000010); //sleep code for stability?
+		joe++;
+		}
+		stop(0);
 return 0;
 }
